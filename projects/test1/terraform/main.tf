3d8cb86db7690312fa85558200da98ab2dc586d8
Mohamed Habib <moe.habib9@gmail.com>
2024-05-16 17:56:34 +0100

    Update main.tf

--- projects/test1/terraform/main.tf
+++ projects/test1/terraform/main.tf
@@ -1,14 +1,3 @@
-# resource "google_storage_bucket" "test1" {
-#   name          = "digger-poc-${var.environment}-test1"
-#   location      = "EUROPE-WEST3"
-#   storage_class = "STANDARD"
-
-#   uniform_bucket_level_access = true
-
-#   labels = {
-#     creator = "terraform"
-#   }
-# }
 
 variable "environment" {
 
